291a0fc09e8b6b5df97c26a99939d2940fbe0f71
ferradjielie <ferradji.elie@outlook.fr>
2025-04-26 17:55:08 +0200

    add code for responsive design  in Nav Home and Themes pages

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,15 +3,17 @@ import styles from './Nav.module.css';
 function Nav() {
   return (
     <nav className={styles.navContent}>
+    <div className={styles.navInner}>
       <div className={styles.logo}>
         <h1>VVV</h1>
       </div>
-
+  
       <ul className={styles.navLinks}>
-        <li><a href="#">Inscription</a></li>
         <li><a href="#">Se connecter</a></li>
       </ul>
-    </nav>
+    </div>
+  </nav>
+  
   );
 }
 
