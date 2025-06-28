a30ad22d6ae00f120009091f77c87baf2c0a47a6
ferradjielie <ferradji.elie@outlook.fr>
2025-06-28 15:50:23 +0200

    add back-end files for favori content

--- back-end/index.js
+++ back-end/index.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth.routes');
-const themeRoutes = require('./routes/theme.routes'); // ✅ Ajouté ici
+const themeRoutes = require('./routes/theme.routes'); 
+const favoriRoutes = require("./routes/favori.routes");
+
 
 const app = express();
 const PORT = 5000;
@@ -14,6 +16,7 @@ app.use(express.json());
 // Routes
 app.use('/api', authRoutes);
 app.use('/api', themeRoutes); 
+app.use("/api", favoriRoutes);
 
 // Démarrage du serveur
 app.listen(PORT, () => {
